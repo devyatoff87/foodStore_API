93796b41f2379aaec9cad88d5b5a65fbd7f85a46
Vladimir Devyatov <vladimir.devyatov@brandad.de>
2024-07-25 15:35:44 +0200

    - sample data added, resolveJsonModule added, server mapping methods added: ged all foods and get food by name

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import foods from '../data/food.data.json';
 
 const app = express();
 //need these cors for development phase
@@ -11,15 +12,26 @@ app.use(cors({
   origin: ['http://localhost:4200']
 }));
 
-app.get("/api/foods", (request, response) => {
-  console.log(request);
-  response.send('I am working')
-});
-
 app.get("", (request, response) => {
   console.log(request);
-  response.send('I am working')
+  response.send('home page')
 });
+
+//get all foods
+app.get("/api/foods", (request, response) => {
+  response.send(foods)
+});
+
+//search food by name
+app.get("/api/foods/search/:searchTerm", (request, response) => {
+  const searchTerm = request.params.searchTerm;
+  const food = foods.filter((f) => {
+    return f.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  response.send(food)
+});
+
 
 app.listen(port, () => {
   console.log("Website is serving successfully on http://localhost:" + port);
